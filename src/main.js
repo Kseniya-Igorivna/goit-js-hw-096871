c82286816076cfc2ec219ed0b88d18d245140c21
Kseniya <159527696+Kseniya-Igorivna@users.noreply.github.com>
2024-07-28 22:58:01 +0300

    8пзж

--- src/main.js
+++ src/main.js
@@ -1,30 +1,87 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Keyboard } from 'swiper/modules';
+import axios from 'axios';
 
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import { listEl, revBtnPrevEl, revBtnNextEl } from './elements';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const faqItems = document.querySelectorAll('.faq-list-ul .list-faq');
+const listEl = document.querySelector('.reviews-swiper-wrapper');
+const revBtnPrevEl = document.querySelector('.reviews-prev-btn');
+const revBtnNextEl = document.querySelector('.reviews-next-btn');
 
-    faqItems.forEach(item => {
-    const question = item.querySelector('.title-list');
-    const toggleBtn = item.querySelector('.toggle-btn');
+async function getReviewsData() {
+  try {
+    const response = await axios.get(
+      'https://portfolio-js.b.goit.study/api/reviews'
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
-    question.addEventListener('click', () => {
-        const answer = item.querySelector('.text-faq');
-        const isOpen = answer.style.display === 'block';
-        closeAllAnswers();
-        if (!isOpen) {
-        answer.style.display = 'block';
-        item.setAttribute('aria-expanded', 'true');
-        }
-    });
-    });
-
-    function closeAllAnswers() {
-    faqItems.forEach(item => {
-        const answer = item.querySelector('.text-faq');
-        answer.style.display = 'none';
-        item.setAttribute('aria-expanded', 'false');
-    });
+async function renderReviews() {
+  try {
+    const reviewsData = await getReviewsData();
+    if (!reviewsData) {
+      listEl.innerHTML = '<li>Not Found</li>';
+      return;
     }
-});
+    const reviewsList = reviewsData
+      .map(
+        review => `
+        
+      <li class="swiper-slide reviews-swiper-slide">
+          <img src="${review.avatar_url}" 
+          loading="lazy" alt="${review.author}" 
+          class="reviews-img" 
+          width="48" height="48">
+          <div class = "reviews-desc">
+          <h3 class="reviews-item-subtitle">${review.author}</h3>
+        <div class="reviews-text">${review.review}</div>
+        </div>
+      </li>
+    `
+      )
+      .join('');
+    listEl.innerHTML = reviewsList;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function initSwiper() {
+  await renderReviews();
+
+  const swiper = new Swiper('.reviews-section .swiper-reviews', {
+    modules: [Navigation, Keyboard],
+    // direction: 'horizontal',
+    spaceBetween: 16,
+    pageUpDown: true,
+    mousewheel: true,
+    navigation: {
+      prevEl: revBtnPrevEl ? revBtnPrevEl : undefined,
+      nextEl: revBtnNextEl ? revBtnNextEl : undefined,
+    },
+    allowTouchMove: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1440: {
+        slidesPerView: 4,
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
+}
+
+initSwiper();
